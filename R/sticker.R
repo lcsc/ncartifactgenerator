cbb4e40f8ce49a9e7355e36866f771337440367f
Eduardo Moreno <eduardo.moreno@ipe.csic.es>
2024-12-17 08:57:08 +0100

    Condition in sticker.R for installing in docker

--- R/sticker.R
+++ R/sticker.R
@@ -10,18 +10,20 @@ pacman::p_load(
 
 p <- ggplot(aes(x = mpg, y = wt), data = mtcars) + geom_point()
 p <- p + theme_void() + theme_transparent()
-sticker(subplot = "./man/figures/frog.png",
-        s_x	= 1,
-        s_y	= 0.7,
-        s_width = 117/128 * 0.4,
-        s_height = 0.4,
-        package = "auto_dag",
-        p_size = 24,
-        p_color = "cornsilk",
-        h_fill = "forestgreen",
-        h_color = "cornsilk",
-        spotlight = TRUE,
-        l_x	= 1,
-        l_y = 1.25,
-        l_width = 6,
-        filename = "./figures/badge.png")
+if (dir.exists('./figures')) {
+  sticker(subplot = "./figures/turtle.png",
+          s_x = 1,
+          s_y = 0.7,
+          s_width = 117/128 * 0.4,
+          s_height = 0.4,
+          package = "auto_dag",
+          p_size = 24,
+          p_color = "cornsilk",
+          h_fill = "forestgreen",
+          h_color = "cornsilk",
+          spotlight = TRUE,
+          l_x = 1,
+          l_y = 1.25,
+          l_width = 6,
+          filename = "./figures/badge.png")
+}
